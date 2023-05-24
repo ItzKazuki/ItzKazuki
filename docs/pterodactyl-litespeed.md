bdc31267e27cbdcf9d85fcf6d949059c11c4d464
Kazuki <ibnu235729@gmail.com>
2023-05-24 11:25:38 +0700

    init upload new site v2 to devlopment

---
+++ docs/pterodactyl-litespeed.md
@@ -0,0 +1,7 @@
+
+# Pterodactyl
+pterodactyl adalah sebuah software yang di kembangkan untuk menjalankan sebuah aplikasi lewat container-container di docker, pterodactyl sangat berguna karna dapat membagi resources dengan rata, hal ini yang menyebabkan peminat download pterodactyl semakin meningkat.
+
+selain itu software ini juga bisa untuk jadi peluang bisnis, karena konsepnya mirip shared hosting. 
+
+## Cara install Pterodactyl (Apache / nginx)
---
+++ mc/script.js
@@ -0,0 +1,58 @@
+/** Fetch account info from a public database */
+document.getElementById('search-bar').onsubmit = function() {
+    // Declare vars
+    var feild = document.getElementById('data');  
+    var results = document.getElementById('results');
+    var result_loading = document.getElementById('result-loading');
+    var result_container = document.getElementById('result-container');
+    var result_img =document.getElementById('result-img');
+    var result_name = document.getElementById('result-name');
+    var result_uuid = document.getElementById('result-uuid');
+    var nameSearch = feild.value.split("-").length == 5;
+
+    // Set up the html before loading
+    feild.disabled = true;
+    results.className = "bg-info";
+    results.style.display = "block";
+    result_loading.style.display = "block";
+    result_container.style.display = "none";
+
+    var request = new XMLHttpRequest();
+    request.onload = function() {
+        // Grab data and assing feilds
+        try {
+            var response = JSON.parse(request.responseText);
+            // If error exists throw the message
+            if (response.code != null) {
+                throw response.msg;
+            }
+            result_img.src = "https://api.year4000.net/avatar/" + response.uuid + "?size=32";
+            result_name.innerHTML = response.username;
+            result_uuid.value = response.uuid;
+            results.className = "bg-success";
+            result_loading.style.display = "none";
+            result_container.style.display = "block";
+        } catch(e) { // When error grabing data show dummy
+            result_img.src = "https://api.year4000.net/avatar/" + feild.value + "?size=32";
+            result_name.innerHTML = !nameSearch ? feild.value : "Unknown Name";
+            result_uuid.value = nameSearch ? feild.value : "Unknown UUID";
+            results.className = "bg-warning";
+            result_loading.style.display = "none";
+            result_container.style.display = "block";
+        } finally {
+            feild.disabled = false;
+        }
+    };
+
+    request.open("GET", "https://api.year4000.net/minecraft/" + feild.value, true);
+    request.send();
+    return false;
+}
+
+/** Select the text feild */
+var highlight = function() {
+    this.select();
+}
+
+document.getElementById('data').onclick = highlight;
+document.getElementById('result-uuid').onclick = highlight;
