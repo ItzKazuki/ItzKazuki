6470ee5c967b4a5cc62485ef25fefb3159c1be7e
ItzKazuki <ibnu235729@gmail.com>
2024-03-24 21:16:53 +0700

    add tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+  daisyui: {
+    themes: ["light"]
+  }
+}
+
